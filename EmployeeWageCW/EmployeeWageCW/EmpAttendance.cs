c7494a7c709a15ef7a26cbe290bd9d0ae8832f4c
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-06-24 16:10:05 +0530

    [Ankita] Add : UC2-Add Part Time Employee Wage

--- EmployeeWageCW/EmployeeWageCW/EmpAttendance.cs
+++ EmployeeWageCW/EmployeeWageCW/EmpAttendance.cs
@@ -8,24 +8,30 @@ namespace EmployeeWageCW
 {
     public class EmpAttendance
     {
-        public int IS_FULLTIME = 1;
-        public int EMP_RATE_PER_HOUR = 20;
-        public void EmployeeCheck()
+        public void CheckEmployee()
         {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 2;
             int empHrs = 0;
             int empWage = 0;
-
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULLTIME)
+            if (empCheck == IS_FULL_TIME)
+            {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
             else
+            {
                 empHrs = 0;
-
-             empWage = empHrs * EMP_RATE_PER_HOUR;
-             Console.WriteLine("Employee wage is: " + empWage);
-
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
