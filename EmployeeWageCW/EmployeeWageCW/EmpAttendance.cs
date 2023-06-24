fc26f424dac1cc2812921b3fb419d8f59e9a53e8
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-06-24 16:18:53 +0530

    [Ankita] Add : UC2-Calculate Emp Wage Using Switch Case

--- EmployeeWageCW/EmployeeWageCW/EmpAttendance.cs
+++ EmployeeWageCW/EmployeeWageCW/EmpAttendance.cs
@@ -8,30 +8,30 @@ namespace EmployeeWageCW
 {
     public class EmpAttendance
     {
-        public void CheckEmployee()
-        {
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int IS_PART_TIME = 2;
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(3);
+           public const int IS_FULL_TIME = 1;
+            public const int EMP_RATE_PER_HOUR = 20;
+            public const int IS_PART_TIME = 2;
+            public void CheckEmployee()
+            {
+                int empHrs = 0;
+                int empWage = 0;
+                Random random = new Random();
+                int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Employee Wage: " + empWage);
             }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage: " + empWage);
         }
-    }
 }
